0602152235cf6f56eae2d57075c9265772da3c1c
MiguelDotL <miguel.d.lozano@gmail.com>
2016-11-11 17:05:20 -0500

    Add user sql query in <ul>

---
+++ php-get-started/select.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP FORM</title>
+  </head>
+  <body>
+    <ul>
+      <?php
+
+        $db = mysqli_connect('localhost', 'root', '', 'php');
+        $sql = 'SELECT * FROM users';
+        $result = mysqli_query($db, $sql);
+
+        foreach($result as $row)  {
+          printf('<li><span style="color: %s">%s (%s)</span></li>',
+            htmlspecialchars($row['color']),
+            htmlspecialchars($row['name']),
+            htmlspecialchars($row['gender'])
+          );
+        }
+
+        mysqli_close($db);
+      ?>
+    </ul>
+  </body>
+</html>
